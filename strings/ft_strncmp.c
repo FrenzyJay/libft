202b1697bd5b77b01f61db9c8f35e5e4ee0398b8
frenzyjay <jay.frenzystuff@gmail.com>
2014-03-24 07:50:07 +0100

    fix strncmp

--- strings/ft_strncmp.c
+++ strings/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jvincent <jvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 13:21:44 by jvincent          #+#    #+#             */
-/*   Updated: 2013/12/29 16:18:54 by jvincent         ###   ########.fr       */
+/*   Updated: 2014/03/24 03:11:36 by jvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(char *s1, char *s2, size_t n)
 	if (s1 && s2)
 	{
 		kount = 0;
-		while ((s1[kount] != '\0' || s2[kount] != '\0') && kount < n)
+		while ((s1[kount] != '\0' || s2[kount] != '\0') && kount <= n)
 		{
 			if (s1[kount] != s2[kount])
 				return (s1[kount] - s2[kount]);
